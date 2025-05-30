8cda1f01700a67460c06b5ee6b8f53ac156ffe59
Vahaz <47829911+Vahaz@users.noreply.github.com>
2025-05-30 17:31:08 +0200

    updated descriptions

--- script.ts
+++ script.ts
@@ -1,3 +1,13 @@
+/**
+ * @description Demo configuration constants
+ * @author Vahaz
+ */
+
+/**
+ * Display an error message to the HTML Element with id "error-container" 
+ * @param msg string
+ * @returns void
+ */
 function showError(msg: string) {
     const container = document.getElementById("error-container");
     if(container === null) return console.log("No Element with ID: error-container");
@@ -7,22 +17,46 @@ function showError(msg: string) {
     console.log(msg);
 }
 
+/**
+ * - Create a WebGL Buffer type. (Opaque Handle)
+ *   - STATIC_DRAW means we wont update often, but often used.
+ *   - ARRAY_BUFFER on the other side indicate the place to store the Array.
+ * 1. First, we attach the Buffer to the CPU.
+ * 2. Add Array to the Buffer.
+ * 3. Clear after use and return the buffer.
+ * @param gl WebGL2RenderingContext, rendering context
+ * @param data ArrayBuffer, data send to buffer
+ * @returns buffer
+ */
 function createStaticVertexBuffer(gl: WebGL2RenderingContext, data: ArrayBuffer) {
-    const buffer = gl.createBuffer(); // Create a WebGL Buffer type. (Opaque Handle)
+    const buffer = gl.createBuffer();
     if(!buffer) { showError("Failed to allocate buffer space"); return null }
-    
-    // STATIC_DRAW → Wont update often, but often used.
-    // ARRAY_BUFFER → Indicate the place to store the Array.
-    
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer); // Attach the Buffer to the CPU.
-    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW); // Add Array to the Buffer.
-    gl.bindBuffer(gl.ARRAY_BUFFER, null); // Clear after use.
-
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
     return buffer
 }
 
-// 4. Input assembler → Read Vertices from GPU Buffer.
-// Tell OpenGL the data type sent (Float, Array, etc)
+/**
+ * Input assembler → Read Vertices from GPU Buffer.
+ * - Tell OpenGL the data type sent (Float, Array, etc)
+ *   - Vertex Shader → Place Vertices in clip space.
+ *   - Fragment Shader → Color the pixels.
+ *   - If Vertex shader and Fragment shader are not present: OpenGL uses default 0 everywhere.
+ * - VertexAttribPointer
+ *   - Index (location)
+ *   - Size (Component per vector)
+ *   - Type
+ *   - IsNormalized (int to floats, for color transform [0, 255] to float [0, 1])
+ *   - Stride (Distance between each vertex in the buffer)
+ *   - Offset (Number of skiped bytes before reading attributes)
+ * @param gl WebGL2RenderingContext, rendering context
+ * @param positionBuffer WebGLBuffer for position
+ * @param colorBuffer WebGLBuffer for color
+ * @param positionAttribute number
+ * @param colorAttribute number
+ * @returns vao (VertexArray)
+ */
 function createTwoBufferVao(
     gl: WebGL2RenderingContext,
     positionBuffer: WebGLBuffer, colorBuffer: WebGLBuffer,
@@ -30,31 +64,23 @@ function createTwoBufferVao(
 ) {
     const vao = gl.createVertexArray();
     if(!vao) { showError("Failed to allocate VAO buffer."); return null; }
-    
     gl.bindVertexArray(vao);
-    gl.enableVertexAttribArray(positionAttribute); // Vertex Shader → Place Vertices in clip space.
-    gl.enableVertexAttribArray(colorAttribute); // Fragment Shader → Color the pixels.
-    // If not present: OpenGL uses default 0 everywhere.
-
+    gl.enableVertexAttribArray(positionAttribute);
+    gl.enableVertexAttribArray(colorAttribute);
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.vertexAttribPointer(positionAttribute, 2, gl.FLOAT, false, 0, 0);
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.vertexAttribPointer(colorAttribute, 3, gl.UNSIGNED_BYTE, true, 0, 0);
-
-    // VertexAttribPointer
-    // - Index (location)
-    // - Size (Component per vector)
-    // - Type
-    // - Normalized? (int to floats, for color transform [0, 255] to float [0, 1])
-    // - Stride (Distance between each vertex in the buffer)
-    // - Offset (Number of skiped bytes before reading attributes)
-
-    // Clear after use
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
     gl.bindVertexArray(null);
     return vao;
 }
 
+/**
+ * Return the WebGL Context from the Canvas Element.
+ * @param canvas HTMLCanvasElement
+ * @returns gl
+ */
 function getContext(canvas: HTMLCanvasElement) {
     const gl = canvas.getContext('webgl2');
     if(!gl) {
@@ -68,32 +94,38 @@ function getContext(canvas: HTMLCanvasElement) {
     return gl;
 }
 
-// [Top-x, Top-y, Left-x, Left-y, Right-x, Right-y]
-// JS uses 64bit Arrays, and CPU prefer 32bits for half numbers.
-// "Default" values, but changed by shaders.
+/**
+ * - [Top-x, Top-y, Left-x, Left-y, Right-x, Right-y]
+ * - JS uses 64bit Arrays, and CPU prefer 32bits for half numbers.
+ * - "Default" values, but changed by shaders.
+ */
 const triangleVertices = new Float32Array([0, 1, -1, -1, 1, -1]);
 
-// Colors !
+/** Colors! */
 const rgbTriangleColors = new Uint8Array([
     255, 0, 0,
     0, 255, 0,
     0, 0, 255
 ]);
+
+/** Colors! */
 const gradientTriangleColors = new Uint8Array([
     229, 47, 15,
     246, 206, 29,
     233, 154, 26
 ]);
 
-// Shader code, need to be placed here to be compiled and used by the GPU.
-// " `#version 300 es " : in this order is mandatory!
-// gl_Position = vec4(x, y, z-sorting [-1;1], w) [built-in]
-// x, y and z are devided by w.
-// Take the canvasSize to devide the finalPosition.
-// The finalPosition: vertexPosition * size + location.
-// to make sure they are in range, we / the finalPos by the canvasSizes
-// ClipPosition is a %, so we need to make it in range from -1 to 1 by adding (*2 - 1)
-// We input a color to the fragment color to pass the color along the shaders.
+/**
+ * Shader code, need to be placed here to be compiled and used by the GPU.
+ * - " `#version 300 es " : in this order is mandatory!
+ * - gl_Position = vec4(x, y, z-sorting [-1;1], w) [built-in]
+ *   - x, y and z are devided by w.
+ * - Take the canvasSize to devide the finalPosition.
+ *   - The finalPosition: vertexPosition * size + location.
+ * - to make sure they are in range, we / the finalPos by the canvasSizes
+ * - ClipPosition is a %, so we need to make it in range from -1 to 1 by adding (*2 - 1)
+ * - We input a color to the fragment color to pass the color along the shaders.
+ */
 const vertexShaderCode = `#version 300 es
 precision mediump float;
 
@@ -114,8 +146,10 @@ void main() {
 }
 `;
 
-// We need to specify here the variables, no built-in inputs.
-// We get the fragmentColor from the vertexShader
+/**
+ * We need to specify here the variables, no built-in inputs.
+ * We get the fragmentColor from the vertexShader
+ */
 const fragmentShaderCode = `#version 300 es
 precision mediump float;
 
@@ -127,10 +161,13 @@ void main() {
 }
 `;
 
-// Create a Class "MovingShape" with a position, velocity, size and vao arguments.
-// The class as a method "update" with dt (delta time) argument.
-// "Update" update the position by adding: position = ((position + velocity) * dt)
-// Position is expressed in pixels and Velocity by pixels per seconds.
+/**
+ * - Create a Class "MovingShape" with a position, velocity, size and vao arguments.
+ * - The class as a method "update" with dt (delta time) argument.
+ * - "Update" update the position by adding: position = ((position + velocity) * dt)
+ * - Position is expressed in pixels and Velocity by pixels per seconds.
+ * @method update
+ */
 class MovingShape {
     constructor(
         public position: [number, number],
@@ -143,22 +180,27 @@ class MovingShape {
     }
 }
 
-//
-// DEMO
-//
+/**
+ * DEMO Main function
+ * @returns void
+ */
 function main() {
-    const canvas = document.getElementById("webgl-canvas"); // Get the canvas
+    const canvas = document.getElementById("webgl-canvas");
     if(!canvas || !(canvas instanceof HTMLCanvasElement)) throw new Error("Failed to get canvas element.");
     
-    const gl = getContext(canvas); // Get the WebGL2 context
+    const gl = getContext(canvas);
 
-    // Create vertex buffers
+    /**
+     * - Create vertex buffers
+     */
     const triangleGeoBuffer = createStaticVertexBuffer(gl, triangleVertices);
     const rgbGeoBuffer = createStaticVertexBuffer(gl, rgbTriangleColors);
     const gradientGeoBuffer = createStaticVertexBuffer(gl, gradientTriangleColors);
     if(!triangleGeoBuffer || !rgbGeoBuffer || !gradientGeoBuffer) return showError("Failed to create vertex buffers.");
 
-    // Setup the vertex shader.
+    /**
+     * - Setup the vertex shader.
+     */
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
     if(vertexShader === null) return showError("No GPU Memory to allocate for Vertex Shader.");
     gl.shaderSource(vertexShader, vertexShaderCode);
@@ -168,7 +210,9 @@ function main() {
         return showError(error || "No shader debug log provided.");
     }
 
-    // Setup the fragment shader.
+    /**
+     * - Setup the fragment shader.
+     */
     const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     if(fragmentShader === null) return showError("No GPU Memory to allocate for Fragment Shader.");
     gl.shaderSource(fragmentShader, fragmentShaderCode);
@@ -178,7 +222,9 @@ function main() {
         return showError(error || "No shader debug log provided.");
     }
 
-    // Required for uniforms.
+    /**
+     * - Required for uniforms.
+     */
     const program = gl.createProgram();
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
@@ -188,18 +234,24 @@ function main() {
         return showError(error || "No program debug log provided.");
     }
 
-    // Get the data from "in" in shader code.
+    /**
+     * - Get the data from "in" in shader code.
+     */
     const vertexPositionAttribute = gl.getAttribLocation(program, 'vertexPosition');
     const vertexColorAttribute = gl.getAttribLocation(program, 'vertexColor');
     if(vertexPositionAttribute < 0 || vertexColorAttribute < 0) return showError("Failed to get Attribute Location for vertexPosition.");
 
-    // We create our Vertex Shader Uniforms variables.
+    /**
+     * - We create our Vertex Shader Uniforms variables.
+     */
     const locationUniform = gl.getUniformLocation(program, 'location');
     const sizeUniform = gl.getUniformLocation(program, 'size');
     const canvasSizeUniform = gl.getUniformLocation(program, 'canvasSize');
     if(locationUniform === null || sizeUniform === null || canvasSizeUniform === null) return showError("Empty Uniforms..");
 
-    // Create buffers vao
+    /**
+     * - Create buffers vao
+     */
     const rgbTriangleVAO = createTwoBufferVao(gl, triangleGeoBuffer, rgbGeoBuffer, vertexPositionAttribute, vertexColorAttribute);
     const gradientTriangleVAO = createTwoBufferVao(gl, triangleGeoBuffer, gradientGeoBuffer, vertexPositionAttribute, vertexColorAttribute);
     if(!rgbTriangleVAO || !gradientTriangleVAO) {
@@ -209,26 +261,38 @@ function main() {
         return null;
     }
 
-    // Setup logical objects in an array to easily loop/create them.
+    /**
+     * - Setup logical objects in an array to easily loop/create them.
+     */
     let shapes: MovingShape[] = [
         new MovingShape([200, 100], [50, 5], 100, rgbTriangleVAO),
         new MovingShape([300, 400], [-50, -5], 50, gradientTriangleVAO)
     ];
 
-    // Add a function to call it each frame.
-    // 1. Output Merger: Merge the shaded pixel fragment with the existing out image.
-    // 2. Rasterizer: Wich pixel are part of the Vertices + Wich part is modified by OpenGL.
-    // 3. GPU Program: Pair Vertex & Fragment shaders.
-    // 4. Uniforms: Setting them (can be set anywhere) (size/loc in pixels (px))
-    // 5. Draw Calls: (w/ Primitive assembly + Triangles)
-    let lastFrameTime = performance.now(); // Time between last frame / now.
+    /**
+     * Time between last frame / now.
+     */
+    let lastFrameTime = performance.now();
+
+    /**
+     * Add a function to call it each frame.
+     * 1. Output Merger: Merge the shaded pixel fragment with the existing out image.
+     * 2. Rasterizer: Wich pixel are part of the Vertices + Wich part is modified by OpenGL.
+     * 3. GPU Program: Pair Vertex & Fragment shaders.
+     * 4. Uniforms: Setting them (can be set anywhere) (size/loc in pixels (px))
+     * 5. Draw Calls: (w/ Primitive assembly + for loop)
+     */
     const frame = function(){
-        // Calculate dt with time in seconds between each frame.
+        /**
+         * Calculate dt with time in seconds between each frame.
+         */
         const thisFrameTime = performance.now();
         const dt = (thisFrameTime - lastFrameTime) / 1000;
         lastFrameTime = thisFrameTime;
 
-        // Update shapes
+        /**
+         * Update shapes
+         */
         shapes.forEach((shape) => {
             shape.update(dt);
         });
@@ -237,10 +301,8 @@ function main() {
         canvas.height = canvas.clientHeight;
         gl.clearColor(0.08, 0.08, 0.08, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
         gl.viewport(0, 0, canvas.width, canvas.height)
         gl.useProgram(program);
-
         gl.uniform2f(canvasSizeUniform, canvas.width, canvas.height);
 
         shapes.forEach((shape) => {
@@ -250,9 +312,15 @@ function main() {
             gl.drawArrays(gl.TRIANGLES, 0, 3);
         });
 
-        requestAnimationFrame(frame); // Loop calls, each time the drawing is ready.
+        /**
+         * Loop calls, each time the drawing is ready.
+         */
+        requestAnimationFrame(frame);
     };
-    requestAnimationFrame(frame); // First call, as soon, as the browser is ready.
+    /**
+     * First call, as soon, as the browser is ready.
+     */
+    requestAnimationFrame(frame);
     
 }
 
